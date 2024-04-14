0b32ba37d421d2f13767ab4a3ca51d253ecaab0a
mayssaabdellaoui123 <58725994+mayssaabdellaoui123@users.noreply.github.com>
2024-04-14 16:54:12 +0100

    Update testfordocker.java

--- PFE/src/test/java/testcases/testfordocker.java
+++ PFE/src/test/java/testcases/testfordocker.java
@@ -12,9 +12,9 @@ import org.testng.annotations.Test;
 import pages.Menu;
 import pages.testfordockerscreen;
 import utilities.BaseFile;
-
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import org.testng.asserts.SoftAssert;
 
 @Listeners({ TestListener.class })
 @Epic("testfordocker tests")
@@ -28,50 +28,55 @@ public class testfordocker extends BaseFile {
         loadConfig();
         startApp();
     } */
-
+   
     @Test
     @Description("Docker Test Case 1")
     public void dockerTestCase1 () throws InterruptedException {
 
-
+       SoftAssert softAssert = new SoftAssert();
         Thread.sleep(1000);
-        Assert.assertEquals("https://www.google.com/","https://www.google.com/");
+        softAssert.assertEquals("https://www.google.com/","https://www.google.com/");
+       softAssert.assertAll();
     }
 
     @Test
     @Description("Docker Test Case 2")
     public void dockerTestCase2 () throws InterruptedException {
 
-
+      SoftAssert softAssert = new SoftAssert();
         Thread.sleep(1000);
-        Assert.assertEquals("https://www.google.com/","https://www.google.com/");
+        softAssert.assertEquals("https://www.google.com/","https://www.google.com/");
+       softAssert.assertAll();
     }
 
     @Test
     @Description("Docker Test Case 3")
     public void dockerTestCase3 () throws InterruptedException {
 
-
+         SoftAssert softAssert = new SoftAssert();
         Thread.sleep(1000);
-        Assert.assertEquals("https://www.google.com/","https://www.google.com/");
+        softAssert.assertEquals("https://www.google.com/","https://www.google.com/");
+       softAssert.assertAll();
     }
 
     @Test
     @Description("Docker Test Case 4")
     public void dockerTestCase4 () throws InterruptedException {
 
-
+        SoftAssert softAssert = new SoftAssert();
         Thread.sleep(1000);
-        Assert.assertEquals("https://www.google.com//","https://www.google.com/");
+        softAssert.assertEquals("https://www.google.com//","https://www.google.com/");
+       softAssert.assertAll();
     }
 
     @Test
     @Description("Docker Test Case 5")
     public void dockerTestCase5 () throws InterruptedException {
 
-
+       SoftAssert softAssert = new SoftAssert();
         Thread.sleep(1000);
-        Assert.assertEquals("https://www.googgle.com//","https://www.google.com/");
+        softAssert.assertEquals("https://www.googgle.com//","https://www.google.com/");
+       softAssert.assertAll();
     }
 
 
